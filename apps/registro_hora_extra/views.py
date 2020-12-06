2ddca9eb12df34c23b7e77337824f4f2f33d0b81
RamonBecker <ramonbecker68@gmail.com>
2020-12-05 21:12:08 -0300

    Listagem de registro de horas

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    UpdateView,
+    DeleteView,
+    CreateView
+)
 
-# Create your views here.
+from .models import RegistroHoraExtra
+
+
+class HoraExtraList(ListView):
+    model = RegistroHoraExtra
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+
+
+
+
